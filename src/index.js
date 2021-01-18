e1d307e7d2d5b835c878718e702ba96a956480fb
ClaytonSiby <claytonsiby@gmail.com>
2021-01-18 20:08:54 +0200

    Configure webpack with babel phaser & assets

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import Phaser from 'phaser';
+
+import { SimpleScene } from './scenes/simple-scene';
+
+const gameConfig = {
+    width: 680,
+    height: 400,
+    scene: SimpleScene
+};
+
+new Phaser.Game(gameConfig);
