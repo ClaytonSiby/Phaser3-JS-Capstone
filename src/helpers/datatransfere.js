13ecc47e40315804ee9ab5379a71599a3712838f
ClaytonSiby <claytonsiby@gmail.com>
2021-02-01 13:17:07 +0200

    Run eslint auto error fix

--- src/helpers/datatransfere.js
+++ src/helpers/datatransfere.js
@@ -1,5 +1,5 @@
-import axios from 'axios'
-import Helpers from './utils'
+import axios from 'axios';
+import Helpers from './utils';
 
 export const postGameScore = async (playerName, score) => {
   const result = await axios
@@ -7,26 +7,26 @@ export const postGameScore = async (playerName, score) => {
       'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/gZ2LeAR8PEPald2qJWzE/scores/',
       {
         user: playerName,
-        score
-      }
+        score,
+      },
     )
     .then(res => res.data)
-    .catch(error => error.response.data)
+    .catch(error => error.response.data);
 
-  return result
-}
+  return result;
+};
 
 export const getGameScore = async () => {
   const request = await axios
     .get(
       'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/gZ2LeAR8PEPald2qJWzE/scores/',
-      {}
+      {},
     )
     .then(res => res.data.result)
     .then(res => Helpers.structureScores(res))
     .catch(err => {
-      err.toJSON()
-    })
+      err.toJSON();
+    });
 
-  return request
-}
+  return request;
+};
